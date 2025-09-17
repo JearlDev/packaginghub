4c95993a33a80d905be2688a0f82d1cfc9b57787
JearlDev <jearlwebdesign@gmail.com>
2025-09-17 17:52:54 +0200

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
-    <p>There are {{ dataSingle }} articles.</p>
+    <h1>Homepage</h1>
   </div>
 </template>
-<script setup>
-// const data = await GqlArticles();
-
-const dataSingle = await GqlArticleSingle({
-  documentId: "r6r9nthsjhh8b6sznjuq25j4",
-  status: "PUBLISHED",
-});
-</script>
+<script setup></script>
