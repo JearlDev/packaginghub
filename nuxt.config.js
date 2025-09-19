b4da2b4b8e844fe2d004d8f352bb57d0ce79d13d
JearlDev <jearlwebdesign@gmail.com>
2025-09-19 08:54:04 +0200

    tailwind install and base styling setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "icon", type: "image/png", href: "/favicon.png" },
+      ],
+    },
+  },
+  css: ["~/assets/css/main.css"],
+  googleFonts: {
+    families: {
+      Montserrat: true,
+      "Open Sans": true,
+    },
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["nuxt-graphql-client"],
-
+  modules: ["nuxt-graphql-client", "@nuxtjs/google-fonts"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   runtimeConfig: {
     public: {
       BACKEND_URL: process.env.GQL_HOST || "http://localhost:1337/graphql",
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <h1>Homepage</h1>
+    <header class="h-40 container">
+      <img
+        class="h-24 object-contain"
+        src="/images/logo.png"
+        alt="packaginghub logo"
+      />
+    </header>
+    <section class="container">
+      <h1>Welcome to PackagingHub</h1>
+      <h2>Your Packaging Partner</h2>
+      <h3>Innovative Solutions for Your Business</h3>
+      <h4>Customized Packaging Solutions</h4>
+      <h5>Eco-Friendly Materials</h5>
+      <h6>Streamlined Processes</h6>
+      <p>Your one-stop solution for all packaging needs.</p>
+    </section>
   </div>
 </template>
 <script setup></script>
