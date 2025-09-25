40bd5f382c5fa5a9269752d785a2a106202d7c21
JearlDev <jearlwebdesign@gmail.com>
2025-09-25 09:21:08 +0200

    updates

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="py-10 lg:py-20 overflow-hidden">
+  <section class="py-12 lg:py-20 overflow-hidden">
     <div
-      class="container relative h-[270px] lg:h-[25vw] xl:h-[23vw] 2xl:h-[24vw]"
+      class="container relative md:h-[270px] lg:h-[25vw] xl:h-[23vw] 2xl:h-[24vw]"
     >
       <div class="max-w-[700px]">
         <h1><span class="text-accent">Packaging </span>Hub</h1>
@@ -15,7 +15,7 @@
       <img
         src="/images/homepage-bg-2.png"
         alt="Packaging Hub"
-        class="absolute rotate-[-33deg] md:rotate-[-10deg] lg:rotate-[unset] -right-16 top-20 md:top-7 lg:top-10 xl:top-0 md:right-0 h-[36vw] md:h-[25vw] object-contain"
+        class="hidden md:block absolute rotate-[-33deg] md:rotate-[-10deg] lg:rotate-[unset] -right-16 top-20 md:top-7 lg:top-10 xl:top-0 md:right-0 h-[36vw] md:h-[25vw] object-contain"
       />
     </div>
   </section>
--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,20 @@
   <div>
     <Header />
     <Hero />
-    <section class="py-10 lg:py-20 bg-secondary/5">
-      <div class="container relative grid grid-cols-5 gap-7 md:gap-10">
+    <section class="pb-12 md:py-12 lg:py-20 bg-secondary/5 relative">
+      <div class="container grid grid-cols-4 gap-7 md:gap-14 items-center">
         <img
+          class="absolute top-0 right-0 md:relative h-72 md:h-[450px] w-full object-cover md:rounded-lg col-span-4 md:col-span-2"
+          src="/images/campaign-creators-e6n7uoEnYbA-unsplash (2).jpg"
+          alt="packaginghub packaging"
+        />
+        <div class="h-72 md:hidden"></div>
+        <!-- <img
           class="w-full md:w-auto md:h-[44vw] lg:h-[40vw] xl:h-[35vw] object-contain col-span-5 md:col-span-2"
           src="/images/about-main.png"
           alt="packaginghub packaging"
-        />
-        <div class="col-span-5 md:col-span-3">
+        /> -->
+        <div class="col-span-4 md:col-span-2">
           <div class="max-w-[700px]">
             <h2>A dynamic print company</h2>
             <p>
@@ -34,10 +40,10 @@
         </div>
       </div>
     </section>
-    <section class="py-10 lg:py-20">
+    <section class="py-12 lg:py-20">
       <div class="container">
         <div class="max-w-[700px] mx-auto text-center">
-          <h2>A dynamic print company</h2>
+          <h2>Featured Products</h2>
           <p>
             All our products are made to the highest standard ensuring that all
             board and inks are FDA® accredited. We also have a FSC®
@@ -71,5 +77,177 @@
         </div>
       </div>
     </section>
+    <section
+      class="bg-cover bg-no-repeat bg-center"
+      style="background-image: url('/images/custom-bg.webp')"
+    >
+      <div class="py-12 lg:py-40 bg-black/50">
+        <div class="container">
+          <div class="max-w-[700px] mx-auto text-center dark-bg">
+            <h2>Custom Designs</h2>
+            <p>
+              All our products are manufactured to each client’s specific needs
+              and designs. We will engineer cartons that fit your product and to
+              the shape that you require. Plain samples will be supplied for
+              your approval before going to production.
+            </p>
+            <div class="pt-7">
+              <a href="#" class="btn btn-primary">Get a Quote</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer>
+      <div class="py-12 lg:py-20 bg-secondary dark-bg">
+        <div class="container">
+          <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-10">
+            <!-- Address & Hours  -->
+            <div class="text-white">
+              <div
+                class="flex flex-col items-center md:items-start text-center md:text-left gap-10"
+              >
+                <div>
+                  <h3 class="h3 font-bold mb-2 !text-accent">Address</h3>
+                  <p>Unit 5 Henton Park,</p>
+                  <p>25 Stella Road,</p>
+                  <p>Montague Gardens.</p>
+                  <p>Cape Town</p>
+                </div>
+
+                <div>
+                  <h3 class="h3 font-bold mb-2 !text-accent">Office Hours</h3>
+                  <p>M-T: 8am – 4.30pm</p>
+                  <p>Friday: 8am – 3pm</p>
+                  <p>S-S: Closed</p>
+                </div>
+              </div>
+            </div>
+
+            <!-- Contact Details -->
+            <div class="text-white">
+              <div
+                class="flex flex-col items-center md:items-start text-center md:text-left gap-10"
+              >
+                <div>
+                  <h3 class="h3 font-bold mb-2 !text-accent">Contact</h3>
+                  <p><a href="tel:+27215553879">+27 21 555 3879</a></p>
+                  <p>
+                    <a href="mailto:kevin@packhub.co.za">kevin@packhub.co.za</a>
+                  </p>
+                </div>
+
+                <div>
+                  <h3 class="h3 font-bold mb-2 !text-accent">Follow Us</h3>
+                  <div class="flex justify-center md:justify-start gap-4 mt-5">
+                    <a
+                      href="#"
+                      class="hover:opacity-80 transition-opacity duration-150"
+                      ><img
+                        height="32"
+                        width="32"
+                        src="/icons/instagram.svg"
+                        alt="instagram icon"
+                    /></a>
+                    <a
+                      href="#"
+                      class="hover:opacity-80 transition-opacity duration-150"
+                      ><img
+                        height="32"
+                        width="32"
+                        src="/icons/facebook.svg"
+                        alt="facebook icon"
+                    /></a>
+                    <a
+                      href="#"
+                      class="hover:opacity-80 transition-opacity duration-150"
+                      ><img
+                        height="32"
+                        width="32"
+                        src="/icons/linkedin.svg"
+                        alt="linkedin icon"
+                    /></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Contact Form -->
+            <div class="text-white md:col-span-2 lg:col-span-2 mt-10 md:mt-0">
+              <h3 class="h3 mb-7 !text-accent">Get in Touch</h3>
+              <form class="space-y-4">
+                <div class="grid md:grid-cols-2 gap-4">
+                  <div>
+                    <label class="hidden mb-2" for="name">Full Name</label>
+                    <input
+                      type="text"
+                      id="name"
+                      class="w-full px-4 py-2 rounded-md bg-white/90 border border-white text-primary placeholder:text-primary/70 focus:placeholder:text-primary/50 focus:outline-accent transition-all duration-150"
+                      placeholder="Full Name"
+                    />
+                  </div>
+                  <div>
+                    <label class="hidden mb-2" for="email">Email Address</label>
+                    <input
+                      type="email"
+                      id="email"
+                      class="w-full px-4 py-2 rounded-md bg-white/90 border border-white text-primary placeholder:text-primary/70 focus:placeholder:text-primary/50 focus:outline-accent transition-all duration-150"
+                      placeholder="Email Address"
+                    />
+                  </div>
+                </div>
+
+                <div class="grid md:grid-cols-2 gap-4">
+                  <div>
+                    <label class="hidden mb-2" for="package-requirements"
+                      >Package Requirements</label
+                    >
+                    <input
+                      type="text"
+                      id="package-requirements"
+                      class="w-full px-4 py-2 rounded-md bg-white/90 border border-white text-primary placeholder:text-primary/70 focus:placeholder:text-primary/50 focus:outline-accent transition-all duration-150"
+                      placeholder="Package Requirements"
+                    />
+                  </div>
+                  <div>
+                    <label class="hidden mb-2" for="company-name"
+                      >Company Name</label
+                    >
+                    <input
+                      type="text"
+                      id="company-name"
+                      class="w-full px-4 py-2 rounded-md bg-white/90 border border-white text-primary placeholder:text-primary/70 focus:placeholder:text-primary/50 focus:outline-accent transition-all duration-150"
+                      placeholder="Company Name"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label class="hidden mb-2" for="notes">Notes</label>
+                  <textarea
+                    id="notes"
+                    rows="4"
+                    class="w-full px-4 py-2 rounded-md bg-white/90 border border-white text-primary placeholder:text-primary/70 focus:placeholder:text-primary/50 focus:outline-accent transition-all duration-150"
+                    placeholder="Other Notes"
+                  ></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-primary py-7 dark-bg">
+        <div class="container">
+          <div class="text-center text-white">
+            &copy; {{ new Date().getFullYear() }} Packaging Hub. Designed &
+            developed by
+            <a href="https://joshearly.co.za" target="_blank">Josh Early</a>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
