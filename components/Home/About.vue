b1108074867c4712995c5983ba415282c7b72067
JearlDev <jearlwebdesign@gmail.com>
2025-11-28 10:31:26 +0200

    seo fix on homepage

--- components/Home/About.vue
+++ components/Home/About.vue
@@ -40,6 +40,7 @@
               v-if="data?.cta[1]"
               :href="data.cta[1].link"
               :target="data.cta[1].openInNewTab ? '_blank' : '_self'"
+              aria-label="Go to About page for more information about Packaging Hub"
               class="btn btn-tertiary"
               >{{ data.cta[1].text }}</a
             >
